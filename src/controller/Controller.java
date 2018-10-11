6418a9a52a7ac440fe2eb4bf0ebce096cec35cc3
Caroline Jarman <carolinerosejarman@gmail.com>
2018-10-11 09:50:35 -0600

    added a print ln

---
+++ src/controller/Controller.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class Controller
+{
+	public void start()
+	{
+		System.out.println("123456789");
+	}
+}
--- src/controller/Runner.java
+++ src/controller/Runner.java
@@ -2,5 +2,8 @@ package controller;
 
 public class Runner
 {
-
+	public static void main (String [] args)
+	{
+		
+	}
 }
